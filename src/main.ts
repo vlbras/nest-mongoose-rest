5405d73c21a6767d27f967271c8e0fd9dffb49e5
vlbras <vlad.bras02@gmail.com>
2024-01-30 17:15:22 +0100

    feat: integrate class-validator and class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
